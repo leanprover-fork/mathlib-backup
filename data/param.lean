363371b3b2318ab7962b535e3b681192c02fc072
Cyril Cohen <cohen@crans.org>
2018-11-06 18:36:20 +0100

    binary parametricity for lean

--- data/param.lean
+++ data/param.lean
@@ -16,8 +16,6 @@ meta def expr.mk_subst_or_app : expr → list expr → expr
 | e []      := e
 | e (x::xs) := (x.instantiate_lam e).mk_subst_or_app xs
 
--- run_cmd tactic.trace $ expr.instantiate_lam `(2) `(λ x : ℕ, x + 1)
-
 private def bid := binder_info.default
 
 meta def expr.strip_lam : expr → nat → option expr
@@ -28,54 +26,74 @@ meta def expr.strip_lam : expr → nat → option expr
 meta def name.ext (ext : string) (x : name) : name :=
   (x.to_string ++ ext : string)
 
-meta def expr.param2
-   (consts : name_map expr)
-   (lconsts : name_map (expr × expr × expr)) :
-   expr → tactic (expr × expr × expr) 
-| (var         db)  := return (var (3 * db), var (3 * db + 1), var (3 * db + 2))
-| (sort        lvl) := 
+meta def param.intro (lconsts : name_map (expr × expr × expr))
+    (x : name) (α0 α1 αR : expr) (body : expr) :
+      tactic ((expr × expr × expr) × name_map (expr × expr × expr) × expr) := do
+  uniq_name0 ← mk_fresh_name,
+  let fresh0 := expr.local_const uniq_name0 (x.ext "0") bid α0,
+  fresh1 ← mk_local_def (x.ext "1") α1,
+  freshR ← mk_local_def (x.ext "R") (αR.mk_subst_or_app [fresh0, fresh1]),
+  let freshs := (fresh0, fresh1, freshR),
+  return (freshs, lconsts.insert uniq_name0 freshs, body.instantiate_var fresh0)
+
+meta def expr.abstract_ : expr →
+ (name → binder_info → expr → expr → expr) → expr → expr
+| e k (expr.local_const n m bi α) := k m bi α (e.abstract_local n)
+| e k _                           := e
+
+meta def expr.param2 (consts : name_map expr) :
+   expr → name_map (expr × expr × expr) → tactic (expr × expr × expr)
+| (var         db)  _ := fail $ "param: cannot translate a var"
+| (sort        lvl) _ :=
   return (sort lvl, sort lvl,
     lam "α0" bid (sort lvl) $ lam "α1" bid (sort lvl) $
     pi "x0" bid (var 1) $ pi "x1" bid (var 1) $ sort lvl)
-| c@(const       x lvls) :=
+| c@(const       x lvls) _ :=
    match consts.find x with
    | some cR := return (c, c, cR)
    | _ := fail $ "param: no translation for constant " ++ to_string x
    end
-| c@(local_const x pry binfo α) := lconsts.find x
-| (app         u v) := do
-  (u0, u1, uR) ← u.param2,
-  (v0, v1, vR) ← v.param2,
+| c@(local_const x pry binfo α) lconsts := lconsts.find x
+| (app         u v) lconsts := do
+  (u0, u1, uR) ← u.param2 lconsts,
+  (v0, v1, vR) ← v.param2 lconsts,
   return (app u0 v0, app u1 v1, uR.mk_app [v0, v1, vR])
-| (lam         x binfo α body) := do
-  (α0, α1, αR) ← α.param2,
-  stripped_αR ← αR.strip_lam 2,
-  (body0, body1, bodyR) ← body.param2,
-  let t0_ := lam (x.ext "0") binfo α0,
-  let t1_ := lam (x.ext "1") binfo α1,
-  let tR := t0_ $ t1_ $ lam (x.ext "R") binfo stripped_αR bodyR,
-  return (t0_ body0, t1_ body1, tR)
-| (pi          x binfo α body) := do
-  (α0, α1, αR) ← α.param2,
-  stripped_αR ← αR.strip_lam 2,
-  (body0, body1, bodyR) ← body.param2,
-  let t0_ := pi (x.ext "0") binfo α0,
-  let t1_ := pi (x.ext "1") binfo α1,
-  let tR := lam ("f0") bid (t0_ body0) $ lam ("f1") bid (t1_ body1) $
-            t0_ $ t1_ $ pi (x.ext "R") binfo stripped_αR $
-            (bodyR.mk_subst_or_app [app (var 4) (var 2), app (var 3) (var 1)]),
-  return (t0_ body0, t1_ body1, tR)
-| (elet        x α val body) := do
-  (α0, α1, αR) ← α.param2,
-  stripped_αR ← αR.strip_lam 2,
-  (val0, val1, valR) ← val.param2,
-  (body0, body1, bodyR) ← body.param2,
-  let t0_ := elet (x.ext "0") α0 val0,
-  let t1_ := elet (x.ext "1") α1 val1,
-  let tR := t0_ $ t1_ $ elet (x.ext "R") stripped_αR valR bodyR,
-  return (t0_ body0, t1_ body1, tR)
-| exp@_ := fail $ "parma: expression " ++ exp.to_string ++ " is not translatable"
+| (lam         x binfo α body) lconsts := do
+  (α0, α1, αR) ← α.param2 lconsts,
+  ((x0, x1, xR), lconstsx, bodyx) ← param.intro lconsts x α0 α1 αR body,
+  (body0, body1, bodyR) ← bodyx.param2 lconstsx,
+  let t0 := body0.abstract_ lam x0,
+  let t1 := body1.abstract_ lam x1,
+  let tR := ((bodyR.abstract_ lam xR).abstract_ lam x1).abstract_ lam x0,
+  return (t0, t1, tR)
+| (pi          x binfo α body) lconsts := do
+  (α0, α1, αR) ← α.param2 lconsts,
+  ((x0, x1, xR), lconstsx, bodyx) ← param.intro lconsts x α0 α1 αR body,
+  (body0, body1, bodyR) ← bodyx.param2 lconstsx,
+  let t0 := body0.abstract_ pi x0,
+  let t1 := body1.abstract_ pi x1,
+  f0 ← mk_local_def "f0" t0,
+  f1 ← mk_local_def "f1" t1,
+  let tR := (((((bodyR.mk_subst_or_app [app f0 x0, app f1 x1]
+     ).abstract_ pi xR).abstract_ pi x1).abstract_ pi x0
+     ).abstract_ lam f1).abstract_ lam f0,
+  return (t0, t1, tR)
+| (elet        x α val body) lconsts := sorry
+  -- [WRONG CODE!!!]
+  -- (α0, α1, αR) ← α.param2,
+  -- (val0, val1, valR) ← val.param2,
+  -- (body0, body1, bodyR) ← body.param2,
+  -- let t0_ := elet (x.ext "0") α0 val0,
+  -- let t1_ := elet (x.ext "1") α1 val1,
+  -- let tR := t0_ $ t1_ $ elet (x.ext "R") stripped_αR valR bodyR,
+  -- return (t0_ body0, t1_ body1, tR)
+  -- [/WRONG CODE!!!]
+| exp@_ _ := fail $ "parma: expression " ++ exp.to_string ++ " is not translatable"
 
-#check has_to_tactic_format
-
-run_cmd expr.param2 mk_name_map mk_name_map `(λ α : Type, α) >>= tactic.trace
+run_cmd do
+  let e := `(λ α : Type, λ x : α, x),
+  let t := `(∀ α : Type, α → α),
+  (t0,t1,tR) ← expr.param2 mk_name_map t mk_name_map,
+  (e0,e1,eR) ← expr.param2 mk_name_map e mk_name_map,
+  teR ← infer_type eR,
+  unify teR (tR.mk_app [e, e])
