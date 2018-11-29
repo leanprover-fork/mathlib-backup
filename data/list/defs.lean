15746565f8d6e3b9e9e6f8d68d191c9675b17454
Keeley Hoek <keeley@hoek.io>
2018-11-29 16:00:54 +1100

    fix(data/list/defs): add copyright header

--- data/list/defs.lean
+++ data/list/defs.lean
@@ -1,3 +1,10 @@
+/-
+Copyright (c) 2014 Parikshit Khanna. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Parikshit Khanna, Jeremy Avigad, Leonardo de Moura, Floris van Doorn, Mario Carneiro
+
+Definitions of list manipulating functions.
+-/
 import data.option logic.basic tactic.interactive
 
 namespace list
