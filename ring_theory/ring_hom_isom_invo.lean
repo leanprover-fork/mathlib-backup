54f9722fe43ee5ecd78a8716507f43ebbda8d422
amswerdlow <37841085+amswerdlow@users.noreply.github.com>
2018-12-19 18:04:24 +0000

    Update ring_hom_isom_invo.lean

--- ring_theory/ring_hom_isom_invo.lean
+++ ring_theory/ring_hom_isom_invo.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2018 Andreas Swerdlow. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Andreas Swerdlow, Kenny Lau
+-/
+
 import algebra.ring data.equiv.basic
 
 variables {R : Type*} {F : Type*}
